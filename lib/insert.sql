6e85bb5f4f1670cb1a494c2bc3aadf93ef5c8470
MarkXtenda <mark.i2p.1998@gmail.com>
2022-08-17 22:51:14 +1000

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+insert into bears (name, age, sex, color, temperament, alive) VALUES
+    ("Mr. Chocolate", 8, "M", "May green", "Playful", 1),
+    ("Rowdy", 15, "F", "Chocolate brown", "Suspicious", 1),
+    ("Tabitha", 7, "M", "Carmine red", "Sociable", 1),
+    ("Sergeant Brown", 9, "F", "Agate grey", "Impulsive", 1),
+    ("Melissa", 28, "M", "Light ivory", "Egocentric",1),
+    ("Grinch", 18, "F", "Grey olive", "Persistent",1),
+    ("Wendy", 16, "F", "Iron grey", "Unhappy",1),
+    (NULL, 28, "F", "Claret violet", "Active",1);
